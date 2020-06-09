f98aff3ec13db4f3dc974aa980fab6e6bed3e0c1
Muhammad Faraz Sohail <43736103+mfg96@users.noreply.github.com>
2020-06-08 20:02:41 -0400

    Add files via upload

---
+++ Lab2.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+#Muhammad Faraz Sohail
+#104986019
+
+
+
+def separator():
+    c=0
+    print("*",end='')
+    while c<23:
+        print("-",end='')
+        c+=1
+    print("*")
+    return
+
+to="| To:\t\t\t|\n| Name: John Doe\t|\n|\t\t\t|\n| Address:\t\t|"
+add="|\t\t\t|\n| University of Windsor |\n| 401 Sunset Avenue\t|\n| Windsor, Ontario\t|\n| Canada N9B3P4\t\t|"
+fr="| From:\t\t\t|\n| Name: Jane Doe\t|\n|\t\t\t|\n| Address:\t\t|"
+add2="|\t\t\t|\n| Planet Jupiter\t|\n| Milky Avenue\t\t|\n| SuperNove, BlackField\t|\n| OutterSpace 26.0@3\t|"
+
+
+
+print("\n")
+separator()
+
+print(to)
+
+separator()
+
+print(add)
+
+separator()
+
+print("\n\n")
+
+separator()
+
+print(fr)
+
+separator()
+
+print(add2)
+
+separator()
+
+print("\n")
