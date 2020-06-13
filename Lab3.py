fd780aab46399d23f0cc9337d05121f5c280e809
Muhammad Faraz Sohail <43736103+mfg96@users.noreply.github.com>
2020-06-13 15:55:50 -0400

    Add files via upload

---
+++ Lab3.py
@@ -0,0 +1,18 @@
+#Muhammad Faraz Sohail (Faraz Sohail on BlackBoard)
+#104986019
+#Lab3
+
+c1=0.10 #conatiners holding litre or less
+c2=0.25 #containers holding more than litre
+
+
+coll1=100 #hundred one litre containers
+coll2=71 #seventy-one 2 litre containers
+coll3=15 #fifteen half litre containers
+
+calc1=c1*(coll1+coll3)
+calc2=c2*coll2
+
+total=calc1+calc2
+
+print("\nRefund for containers of litre and less of Coke = $"+str(round(calc1,2))+"\n Refund for containers more than litre of Pepsi = $"+str(round(calc2,2))+"\n\t\t\t\t   Total Refund = $"+str(round(total,2)),end='\n\n')
